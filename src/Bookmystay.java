40f0fcb10b4c1ef005896c84418d04725a799bfd
chethan871 <cj8719@srmist.edu.in>
2026-03-12 10:10:52 +0530

    UC1

--- src/Bookmystay.java
+++ src/Bookmystay.java
@@ -1,1 +1,9 @@
 public class Bookmystay {
+    public static void main(String[] args) {
+        String appName = "Book My Stay - Hotel Booking System";
+        String version = "Version 1.0";
+
+        System.out.println("Welcome to " + appName);
+        System.out.println(version);
+    }
+}
